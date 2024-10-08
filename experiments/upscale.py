3abf4cc75e2a02907c9f6347f8004d06f14265d2
B Horn <b@horn.uk>
2024-10-08 16:25:40 +0100

    Using the webcam for the upscaling test
    Signed-off-by: B Horn <b@horn.uk>

--- experiments/upscale.py
+++ experiments/upscale.py
@@ -1,30 +1,48 @@
-import sys
-from tqdm import tqdm
+import cv2 as cv
 import onnxruntime
 import numpy as np
-from PIL import Image
-import matplotlib.pyplot as plt
 
 
-def to_numpy(img):
-    return np.asarray(img).astype(np.float32) / 255.0
+session = onnxruntime.InferenceSession(
+    'models/4xNomos2_hq_dat2_fp32.onnx',
+    providers=['CPUExecutionProvider']
+)
+
+cap = cv.VideoCapture(0)
+if not cap.isOpened():
+    print("Cannot open camera")
+    exit()
 
 
-session = onnxruntime.InferenceSession('models/4xNomos2_hq_dat2_fp32.onnx')
+def upscale(img):
+    # this still has clipping issues?
+    img = img.transpose(2, 0, 1) / 255.0
 
-img = Image.open(sys.argv[1]).resize((128, 128))
-img = to_numpy(img)
-img = img.transpose(2, 0, 1)
-
-print('here')
-
-for i in tqdm(range(4)):
     res = session.run([], {'input': [img]})[0][0]
-import matplotlib.pyplot as plt
 
-print(res.shape)
-res = res.transpose(1, 2, 0)
-print(res.shape)
+    res = res.transpose(1, 2, 0) * 255.0
+    res = res.astype(np.uint8)
+    return res
 
-plt.imshow(res.astype(np.float32))
-plt.show()
+
+def main():
+    while True:
+        # Capture frame-by-frame
+        ret, frame = cap.read()
+
+        # if frame is read correctly ret is True
+        if not ret:
+            print("Can't receive frame (stream end?). Exiting ...")
+            break
+        # Our operations on the frame come here
+        img = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
+        img = cv.resize(frame, (128, 128))
+        img = upscale(img)
+        # Display the resulting frame
+        cv.imshow('frame', img)
+        if cv.waitKey(1) == ord('q'):
+            break
+
+
+if __name__ == "__main__":
+    main()
